12884faa03c0b6b1230cf2fd5f24456759185888
stevevovchyna <steve.vovchyna@gmail.com>
2020-01-31 14:19:47 +0200

    Initial Commit

---
+++ TipstartTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipstartTest
+//
+//  Created by Steve Vovchyna on 31.01.2020.
+//  Copyright © 2020 Steve Vovchyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
